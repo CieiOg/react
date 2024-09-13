7decb1cd31eef2a5f55366863e220daacdf09396
ciei <cieipmail@gmail.com>
2024-09-13 16:32:36 -0300

    projeto 2 completo

--- segundo-projeto/src/App.jsx
+++ segundo-projeto/src/App.jsx
@@ -1,27 +1,37 @@
-import './App.css';
-import FirstComponent from './components/FirstComponent';
-
+import FirstComponent from './components/FirstComponent'
 import floresta from './assets/floresta.jpg'
 import OutroComponent from './components/OutroComponent';
+import Component3 from './components/Component3';
+import './App.css'
 
-let n = 11
+
 
 function App() {
+  let n = 11
+  let classDiferente = false
   return (
     <div className="App">
-      <FirstComponent />
+
+      <FirstComponent/>
       <OutroComponent/>
 
       <img src= {floresta} alt='' height={'400px'} width={'400px'}></img>
 
-      <p className='douradoPreto'>paragrafo do jsx dourado</p>
+      {/* <p className='douradoPreto'>paragrafo do jsx dourado</p> */}
 
-      <p style= {{color: 'red', backgroundColor: 'purple', height: '100px', display: 'flex', alignItems: 'center', justifyContent: 'center', fontSize: '40px'}} >paragrafo do jsx estilização in-line</p>
+      {/* <p style= {{color: 'red', backgroundColor: 'purple', height: '100px', display: 'flex', alignItems: 'center', justifyContent: 'center', fontSize: '40px'}} >paragrafo do jsx estilização in-line</p> */}
 
-      <h3 style={n > 10 ? ({background: 'green'}) : ({background: 'red'})}>Cor muda de acordo com o IF</h3>
+      {/* <h3 style={n > 10 ? ({background: 'green'}) : ({background: 'red'})}>Cor muda de acordo com o IF</h3>
       
-      <h3 style={n < 10 ? ({background: 'green'}) : ({background: 'red'})}>Cor muda de acordo com o IF</h3>
+      <h3 style={n < 10 ? ({background: 'green'}) : ({background: 'red'})}>Cor muda de acordo com o IF</h3> */}
 
+
+        <h2 className={classDiferente ? 'greenC' : 'redC'}>
+          Texto a ter a classe mudada de acordo com a verificação
+        </h2>
+
+        <Component3/>
+        
     </div>
   );
 }
---
+++ segundo-projeto/src/components/Component3.jsx
@@ -0,0 +1,14 @@
+import styles from './Component3.module.css'
+
+const Component3 = () => {
+  return (
+    <div>
+      <h1 className={styles.tittle3}>Texto Component3</h1>
+
+      
+
+    </div>
+  )
+}
+
+export default Component3
